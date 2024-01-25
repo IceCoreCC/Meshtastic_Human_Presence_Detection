a0e0035c634d381cc7cf0a2e3a0cf2e5aeb4d03a
Norman Azar <normanazar@yahoo.de>
2024-01-25 18:37:56 +0100

    Folder structure + README

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+## Setup
+
+    `npm install`
+    To install used packages.
+
+## Run
+
+    `npm run server` to start WebSocket Server on PORT 8080
+    `npm run client` to start Python Client (Requires: Connected Meshtastic Device, Running InfluxDB Server on PORT 8086, Running WebSocket Server)
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
+/*
+  WebSocket Server for connecting python client to frontend/website.
+  Sends any recieved message to all connected websocket clients.
+  Python Client (Listening to meshtastic messages) -> WebSocket Server (this) -> Frontend/Website
+*/
+
 const { WebSocketServer } = require("ws");
 
 const sockserver = new WebSocketServer({ port: 8080 });
 const sockets = [];
 
-sockserver.on("connection", (ws, request, client) => {
+sockserver.on("connection", (ws) => {
   ws.send("Connection established");
   console.log("New user connected: ", ws);
   sockets.push(ws);
